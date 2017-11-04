0be4a07453dd8638427168bfbdf9630910f75aac
jingjiajie <932166095@qq.com>
2017-11-04 23:33:15 +0800

    添加项目文件。

---
+++ BLL/Program.fs
@@ -0,0 +1,10 @@
+﻿// 在 http://fsharp.org 上了解有关 F# 的详细信息
+// 请参阅“F# 教程”项目以获取更多帮助。
+
+open System
+
+[<EntryPoint>]
+let main argv = 
+    printfn "%A" "Hello World"
+    Console.ReadLine() |> ignore
+    0 // 返回整数退出代码
