8322eb0830630653acdbb338faa019b986f204e6
jingjiajie <932166095@qq.com>
2017-11-25 14:15:30 +0800

    测试

--- UI/MainForm.Designer.cs
+++ UI/MainForm.Designer.cs
@@ -39,7 +39,7 @@
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(531, 299);
+            this.button1.Location = new System.Drawing.Point(135, 185);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(232, 76);
             this.button1.TabIndex = 0;
