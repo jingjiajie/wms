7b08d70eaed8a93e393f306e57f4c216fed61b64
hsjjaaaa <a098100@163.com>
2017-12-12 14:03:30 +0800

    123

--- UI/FormStockInfo/StockInfoCheckTicketViewMetaData.cs
+++ UI/FormStockInfo/StockInfoCheckTicketViewMetaData.cs
@@ -9,13 +9,13 @@ namespace WMS.UI
     {
         private static KeyName[] keyNames = {
             new KeyName(){Key="ID",Name="ID",Visible=false,Editable=false,Save=false},
-            new KeyName(){Key="StockCheckTicketID",Name="盘点单号",Visible=false,Editable=true},
-            new KeyName(){Key="CreateTime",Name="创建时间",Editable=false,Save=false},
-            new KeyName(){Key="CreateUserUsername",Name="创建用户",Editable=false,Save=false},
-            new KeyName(){Key="PersonInCharge",Name="责任人",Editable=false,Save=false},
-            new KeyName(){Key="LastUpdateUserUsername",Name="最后修改用户",Editable=false,Save=false},
-            new KeyName(){Key="LastUpdateTime",Name="最后修改时间",Editable=false,Save=false},
-            new KeyName(){Key="State",Name="状态",Editable=false,Save=false},
+            new KeyName(){Key="StockCheckTicketID",Name="盘点单号",Visible=true ,Editable=true},
+            new KeyName(){Key="CreateTime",Name="创建时间",Visible=true,Editable=true,Save=false},
+            new KeyName(){Key="CreateUserUsername",Name="创建用户",Visible=true,Editable=true,Save=false},
+            new KeyName(){Key="PersonInCharge",Name="责任人",Visible=true,Editable=true,Save=false},
+            new KeyName(){Key="LastUpdateUserUsername",Name="最后修改用户",Visible=true,Editable=true,Save=false},
+            new KeyName(){Key="LastUpdateTime",Name="最后修改时间",Visible=true,Editable=true,Save=false},
+            new KeyName(){Key="State",Name="状态",Visible=true,Editable=true,Save=false},
 
         };
 
