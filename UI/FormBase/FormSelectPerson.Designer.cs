6938ce24e61ed50521a77253adc1b9739efacdaa
陈光晃 <wingmonth@outlook.com>
2018-01-09 16:27:51 +0800

    12

---
+++ UI/FormBase/FormSelectPerson.Designer.cs
@@ -0,0 +1,184 @@
+﻿namespace WMS.UI
+{
+    partial class FormSelectPerson
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FormSelectPerson));
+            this.reoGridControlMain = new unvell.ReoGrid.ReoGridControl();
+            this.statusStrip1 = new System.Windows.Forms.StatusStrip();
+            this.toolStripStatusLabel1 = new System.Windows.Forms.ToolStripStatusLabel();
+            this.labelStatus = new System.Windows.Forms.ToolStripStatusLabel();
+            this.toolStrip1 = new System.Windows.Forms.ToolStrip();
+            this.toolStripLabel1 = new System.Windows.Forms.ToolStripLabel();
+            this.textBoxPersonName = new System.Windows.Forms.ToolStripTextBox();
+            this.buttonSearch = new System.Windows.Forms.ToolStripButton();
+            this.toolStripSeparator2 = new System.Windows.Forms.ToolStripSeparator();
+            this.buttonSelect = new System.Windows.Forms.ToolStripButton();
+            this.statusStrip1.SuspendLayout();
+            this.toolStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // reoGridControlMain
+            // 
+            this.reoGridControlMain.BackColor = System.Drawing.Color.White;
+            this.reoGridControlMain.ColumnHeaderContextMenuStrip = null;
+            this.reoGridControlMain.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.reoGridControlMain.LeadHeaderContextMenuStrip = null;
+            this.reoGridControlMain.Location = new System.Drawing.Point(0, 28);
+            this.reoGridControlMain.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.reoGridControlMain.Name = "reoGridControlMain";
+            this.reoGridControlMain.Readonly = true;
+            this.reoGridControlMain.RowHeaderContextMenuStrip = null;
+            this.reoGridControlMain.Script = null;
+            this.reoGridControlMain.SheetTabContextMenuStrip = null;
+            this.reoGridControlMain.SheetTabNewButtonVisible = true;
+            this.reoGridControlMain.SheetTabVisible = true;
+            this.reoGridControlMain.SheetTabWidth = 93;
+            this.reoGridControlMain.ShowScrollEndSpacing = true;
+            this.reoGridControlMain.Size = new System.Drawing.Size(665, 383);
+            this.reoGridControlMain.TabIndex = 14;
+            this.reoGridControlMain.Text = "reoGridControl1";
+            // 
+            // statusStrip1
+            // 
+            this.statusStrip1.ImageScalingSize = new System.Drawing.Size(32, 32);
+            this.statusStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripStatusLabel1,
+            this.labelStatus});
+            this.statusStrip1.Location = new System.Drawing.Point(0, 411);
+            this.statusStrip1.Name = "statusStrip1";
+            this.statusStrip1.Padding = new System.Windows.Forms.Padding(0, 0, 11, 0);
+            this.statusStrip1.Size = new System.Drawing.Size(665, 25);
+            this.statusStrip1.TabIndex = 15;
+            this.statusStrip1.Text = "statusStrip1";
+            // 
+            // toolStripStatusLabel1
+            // 
+            this.toolStripStatusLabel1.Name = "toolStripStatusLabel1";
+            this.toolStripStatusLabel1.Size = new System.Drawing.Size(54, 20);
+            this.toolStripStatusLabel1.Text = "状态：";
+            // 
+            // labelStatus
+            // 
+            this.labelStatus.Name = "labelStatus";
+            this.labelStatus.Size = new System.Drawing.Size(84, 20);
+            this.labelStatus.Text = "选择人员";
+            // 
+            // toolStrip1
+            // 
+            this.toolStrip1.BackgroundImage = global::WMS.UI.Properties.Resources.bottonW_q;
+            this.toolStrip1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.toolStrip1.Font = new System.Drawing.Font("Microsoft YaHei UI", 9F);
+            this.toolStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.toolStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripLabel1,
+            this.textBoxPersonName,
+            this.buttonSearch,
+            this.toolStripSeparator2,
+            this.buttonSelect});
+            this.toolStrip1.Location = new System.Drawing.Point(0, 0);
+            this.toolStrip1.Name = "toolStrip1";
+            this.toolStrip1.Size = new System.Drawing.Size(665, 28);
+            this.toolStrip1.TabIndex = 5;
+            this.toolStrip1.Text = "toolStrip1";
+            // 
+            // toolStripLabel1
+            // 
+            this.toolStripLabel1.Name = "toolStripLabel1";
+            this.toolStripLabel1.Size = new System.Drawing.Size(99, 25);
+            this.toolStripLabel1.Text = "人员名称：";
+            // 
+            // textBoxPersonName
+            // 
+            this.textBoxPersonName.Name = "textBoxPersonName";
+            this.textBoxPersonName.Size = new System.Drawing.Size(135, 28);
+            this.textBoxPersonName.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.textBoxPersonName_KeyPress);
+            this.textBoxPersonName.Click += new System.EventHandler(this.textBoxPersonName_Click);
+            // 
+            // buttonSearch
+            // 
+            this.buttonSearch.Image = ((System.Drawing.Image)(resources.GetObject("buttonSearch.Image")));
+            this.buttonSearch.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.buttonSearch.Name = "buttonSearch";
+            this.buttonSearch.Size = new System.Drawing.Size(63, 25);
+            this.buttonSearch.Text = "查询";
+            this.buttonSearch.Click += new System.EventHandler(this.buttonSearch_Click);
+            // 
+            // toolStripSeparator2
+            // 
+            this.toolStripSeparator2.AutoSize = false;
+            this.toolStripSeparator2.Name = "toolStripSeparator2";
+            this.toolStripSeparator2.Size = new System.Drawing.Size(15, 28);
+            // 
+            // buttonSelect
+            // 
+            this.buttonSelect.Image = ((System.Drawing.Image)(resources.GetObject("buttonSelect.Image")));
+            this.buttonSelect.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.buttonSelect.Name = "buttonSelect";
+            this.buttonSelect.Size = new System.Drawing.Size(93, 25);
+            this.buttonSelect.Text = "确认选择";
+            this.buttonSelect.Click += new System.EventHandler(this.buttonSelect_Click);
+            // 
+            // FormSelectPerson
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(665, 436);
+            this.Controls.Add(this.reoGridControlMain);
+            this.Controls.Add(this.statusStrip1);
+            this.Controls.Add(this.toolStrip1);
+            this.Margin = new System.Windows.Forms.Padding(2);
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "FormSelectPerson";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "选择人员";
+            this.Load += new System.EventHandler(this.FormSelectPerson_Load);
+            this.statusStrip1.ResumeLayout(false);
+            this.statusStrip1.PerformLayout();
+            this.toolStrip1.ResumeLayout(false);
+            this.toolStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private unvell.ReoGrid.ReoGridControl reoGridControlMain;
+        private System.Windows.Forms.StatusStrip statusStrip1;
+        private System.Windows.Forms.ToolStripStatusLabel toolStripStatusLabel1;
+        private System.Windows.Forms.ToolStripStatusLabel labelStatus;
+        private System.Windows.Forms.ToolStrip toolStrip1;
+        private System.Windows.Forms.ToolStripLabel toolStripLabel1;
+        private System.Windows.Forms.ToolStripTextBox textBoxPersonName;
+        private System.Windows.Forms.ToolStripButton buttonSearch;
+        private System.Windows.Forms.ToolStripButton buttonSelect;
+        private System.Windows.Forms.ToolStripSeparator toolStripSeparator2;
+    }
+}
---
+++ UI/FormBase/FormSelectPerson.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using unvell.ReoGrid;
+using WMS.DataAccess;
+using System.Threading;
+
+namespace WMS.UI
+{
+    public partial class FormSelectPerson : Form
+    {
+        private Action<int> selectFinishCallback = null;
+        private int defaultPersonID = -1;
+
+        public FormSelectPerson()
+        {
+            InitializeComponent();
+        }
+
+        public void SetSelectFinishCallback(Action<int> selectFinishedCallback)
+        {
+            this.selectFinishCallback = selectFinishedCallback;
+        }
+
+        private void InitComponents()
+        {
+            //初始化表格
+            var worksheet = this.reoGridControlMain.Worksheets[0];
+            worksheet.SelectionMode = WorksheetSelectionMode.Row;
+            for (int i = 0; i < FormBase.BasePersonMetaData.KeyNames.Length; i++)
+            {
+                worksheet.ColumnHeaders[i].Text = FormBase.BasePersonMetaData.KeyNames[i].Name;
+                worksheet.ColumnHeaders[i].IsVisible = FormBase.BasePersonMetaData.KeyNames[i].Visible;
+            }
+            worksheet.Columns = FormBase.BasePersonMetaData.KeyNames.Length; //限制表的长度
+        }
+
+        private void FormSelectPerson_Load(object sender, EventArgs e)
+        {
+            InitComponents();
+            if (this.defaultPersonID != -1)
+            {
+                WMSEntities wmsEntities = new WMSEntities();
+                this.textBoxPersonName.Text = (from s in wmsEntities.PersonView where s.ID == defaultPersonID select s.Name).FirstOrDefault();
+                this.Search();
+            }
+        }
+
+        private void buttonSearch_Click(object sender, EventArgs e)
+        {
+            this.Search();
+        }
+
+        private void Search()
+        {
+            string personName = this.textBoxPersonName.Text;
+            this.labelStatus.Text = "正在搜索...";
+            new Thread(new ThreadStart(() =>
+            {
+                WMSEntities wmsEntities = new WMSEntities();
+                try
+                {
+                   
+                    PersonView[] personViews = (from s in wmsEntities.PersonView
+                                                    where s.Name.Contains(personName)
+                                                    orderby s.Name ascending
+                                                    select s).ToArray();
+                    this.Invoke(new Action(() =>
+                    {
+                        var worksheet = this.reoGridControlMain.Worksheets[0];
+                        worksheet.DeleteRangeData(RangePosition.EntireRange);
+                        for (int i = 0; i < personViews.Length; i++)
+                        {
+                            PersonView curPersonView = personViews[i];
+                            object[] columns = Utilities.GetValuesByPropertieNames(curPersonView, (from kn in FormBase.BasePersonMetaData.KeyNames select kn.Key).ToArray());
+                            for (int j = 0; j < worksheet.Columns; j++)
+                            {
+                                worksheet[i, j] = columns[j] == null ? "" : columns[j].ToString();
+                            }
+                        }
+                        if (personViews.Length == 0)
+                        {
+                            worksheet[0, 2] = "没有查询到符合条件的记录";
+                        }
+                        this.labelStatus.Text = "搜索完成";
+                    }));
+                }
+                catch (Exception)
+                {
+                    MessageBox.Show("修改失败，请检查网络连接", "提示", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    return;
+                }
+                
+            })).Start();
+        }
+
+        private void buttonSelect_Click(object sender, EventArgs e)
+        {
+            this.SelectItem();
+        }
+
+        private void SelectItem()
+        {
+            int[] ids = Utilities.GetSelectedIDs(this.reoGridControlMain);
+            if (ids.Length != 1)
+            {
+                MessageBox.Show("请选择一项");
+                return;
+            }
+            this.selectFinishCallback?.Invoke(ids[0]);
+            this.Close();
+        }
+
+        private void textBoxPersonName_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBoxPersonName_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if(e.KeyChar == 13)
+            {
+                this.Search();
+            }
+        }
+    }
+}
