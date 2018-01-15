f597eb51e7f1ccfbfc951452489237f1a705e0e6
陈光晃 <wingmonth@outlook.com>
2018-01-15 17:00:00 +0800

    21312

--- UI/FormBase/FormSelectPerson.cs
+++ UI/FormBase/FormSelectPerson.cs
@@ -14,12 +14,17 @@ namespace WMS.UI
 {
     public partial class FormSelectPerson : Form,IFormSelect
     {
-        private Action<int> selectFinishCallback = null;
+        private PagerWidget<PersonView> pagerWidget = null;
         private int defaultPersonID = -1;
+        private int projectID = -1;
+        private int warehouseID = -1;
+        static Point staticPos = new Point(-1, -1);
+        private Action<int> selectFinishCallback = null;
 
         public FormSelectPerson()
         {
             InitializeComponent();
+            //this.defaultPersonID = personid;
         }
 
         public void SetSelectFinishedCallback(Action<int> selectFinishedCallback)
@@ -29,24 +34,34 @@ namespace WMS.UI
 
         private void InitComponents()
         {
-            //初始化表格
-            var worksheet = this.reoGridControlMain.Worksheets[0];
-            worksheet.SelectionMode = WorksheetSelectionMode.Row;
-            for (int i = 0; i < FormBase.BasePersonMetaData.KeyNames.Length; i++)
-            {
-                worksheet.ColumnHeaders[i].Text = FormBase.BasePersonMetaData.KeyNames[i].Name;
-                worksheet.ColumnHeaders[i].IsVisible = FormBase.BasePersonMetaData.KeyNames[i].Visible;
-            }
-            worksheet.Columns = FormBase.BasePersonMetaData.KeyNames.Length; //限制表的长度
+            this.textBoxPersonName.Text = "";
+            this.pagerWidget = new PagerWidget<PersonView>(this.reoGridControlMain, FormBase.BasePersonMetaData.KeyNames, this.projectID, this.warehouseID);
+            this.panelPagerWidget.Controls.Add(this.pagerWidget);
+            this.pagerWidget.Show();
+
         }
 
         private void FormSelectPerson_Load(object sender, EventArgs e)
         {
+
             InitComponents();
             if (this.defaultPersonID != -1)
             {
-                WMSEntities wmsEntities = new WMSEntities();
-                this.textBoxPersonName.Text = (from s in wmsEntities.PersonView where s.ID == defaultPersonID select s.Name).FirstOrDefault();
+                try
+                {
+                    WMSEntities wmsEntities = new WMSEntities();
+                    this.textBoxPersonName.Text = (from s in wmsEntities.PersonView where s.ID == defaultPersonID select s.Name).FirstOrDefault();
+                    this.Search(defaultPersonID);
+                }
+                catch
+                {
+                    MessageBox.Show("加载数据失败，请检查网络连接", "提示", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    this.Close();
+                    return;
+                }
+            }
+            else
+            {
                 this.Search();
             }
         }
@@ -56,47 +71,20 @@ namespace WMS.UI
             this.Search();
         }
 
-        private void Search()
+        private void Search(int selectID = -1)
         {
-            string personName = this.textBoxPersonName.Text;
-            this.labelStatus.Text = "正在搜索...";
-            new Thread(new ThreadStart(() =>
+            if (this.textBoxPersonName.Text != "")
             {
-                WMSEntities wmsEntities = new WMSEntities();
-                try
-                {
-                   
-                    PersonView[] personViews = (from s in wmsEntities.PersonView
-                                                    where s.Name.Contains(personName)
-                                                    orderby s.Name ascending
-                                                    select s).ToArray();
-                    this.Invoke(new Action(() =>
-                    {
-                        var worksheet = this.reoGridControlMain.Worksheets[0];
-                        worksheet.DeleteRangeData(RangePosition.EntireRange);
-                        for (int i = 0; i < personViews.Length; i++)
-                        {
-                            PersonView curPersonView = personViews[i];
-                            object[] columns = Utilities.GetValuesByPropertieNames(curPersonView, (from kn in FormBase.BasePersonMetaData.KeyNames select kn.Key).ToArray());
-                            for (int j = 0; j < worksheet.Columns; j++)
-                            {
-                                worksheet[i, j] = columns[j] == null ? "" : columns[j].ToString();
-                            }
-                        }
-                        if (personViews.Length == 0)
-                        {
-                            worksheet[0, 2] = "没有查询到符合条件的记录";
-                        }
-                        this.labelStatus.Text = "搜索完成";
-                    }));
-                }
-                catch (Exception)
-                {
-                    MessageBox.Show("修改失败，请检查网络连接", "提示", MessageBoxButtons.OK, MessageBoxIcon.Error);
-                    return;
-                }
-                
-            })).Start();
+                string value = this.textBoxPersonName.Text;
+                this.pagerWidget.ClearCondition();
+                this.pagerWidget.AddCondition("人员姓名", value);
+                this.pagerWidget.Search(false, selectID);
+            }
+            else
+            {
+                this.pagerWidget.ClearCondition();
+                this.pagerWidget.Search(false, selectID);
+            }
         }
 
         private void buttonSelect_Click(object sender, EventArgs e)
@@ -133,6 +121,30 @@ namespace WMS.UI
         {
             this.Hide();
             e.Cancel = true;
+        }
+  
+
+
+        private void textBoxPersonName_VisbleChanged(object sender, EventArgs e)
+        {
+            if (this.Visible == false)
+            {
+                staticPos = this.Location;
+            }
+            else
+            {
+                if (staticPos != new Point(-1, -1))
+                {
+                    this.Location = staticPos;
+                }
+                int[] ids = Utilities.GetSelectedIDs(this.reoGridControlMain);
+                int id = -1;
+                if (ids.Length > 0)
+                {
+                    id = ids[0];
+                }
+                this.pagerWidget.Search(true, id);
+            }
         }
     }
 }
