27de2a971abda2234d2282a5c7ab260f6d02b3ee
a098100 <a098100@163.com>
2018-01-21 18:30:47 +0800

    123

--- UI/FormRemind/FormSupplyRemind.Designer.cs
+++ UI/FormRemind/FormSupplyRemind.Designer.cs
@@ -28,17 +28,37 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
             this.textBox1 = new System.Windows.Forms.TextBox();
+            this.tableLayoutPanel1.SuspendLayout();
             this.SuspendLayout();
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.ColumnCount = 3;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 3F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 94F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 3F));
+            this.tableLayoutPanel1.Controls.Add(this.textBox1, 1, 1);
+            this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(0, 0);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 3;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 5F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 90F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 5F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(667, 297);
+            this.tableLayoutPanel1.TabIndex = 0;
             // 
             // textBox1
             // 
             this.textBox1.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.textBox1.Location = new System.Drawing.Point(0, 0);
+            this.textBox1.Location = new System.Drawing.Point(23, 17);
             this.textBox1.Multiline = true;
             this.textBox1.Name = "textBox1";
             this.textBox1.ReadOnly = true;
-            this.textBox1.Size = new System.Drawing.Size(667, 297);
+            this.textBox1.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.textBox1.Size = new System.Drawing.Size(620, 261);
             this.textBox1.TabIndex = 0;
             // 
             // FormSupplyRemind
@@ -46,20 +66,20 @@
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(667, 297);
-            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.tableLayoutPanel1);
             this.Name = "FormSupplyRemind";
             this.Text = "FormSupplyRemind";
             this.TopMost = true;
             this.Load += new System.EventHandler(this.FormSupplyRemind_Load);
-            this.Paint += new System.Windows.Forms.PaintEventHandler(this.FormSupplyRemind_Paint);
-            this.Resize += new System.EventHandler(this.FormSupplyRemind_Resize);
+            this.tableLayoutPanel1.ResumeLayout(false);
+            this.tableLayoutPanel1.PerformLayout();
             this.ResumeLayout(false);
-            this.PerformLayout();
 
         }
 
         #endregion
 
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
         private System.Windows.Forms.TextBox textBox1;
     }
 }
--- UI/FormRemind/FormSupplyRemind.cs
+++ UI/FormRemind/FormSupplyRemind.cs
@@ -29,52 +29,18 @@ namespace WMS.UI
             this.Left = (int)(a * Screen.PrimaryScreen.Bounds.Width);
             this.Width = 200;
             this.Height = 75;
-           
+            this.textBox1.Text = "";
+          
+
+
         }
 
 
 
-        private void FormSupplyRemind_Resize(object sender, EventArgs e)
-        {
-            this.Region = null;
-            SetWindowRegion();
-
-        }
-
-        private void FormSupplyRemind_Paint(object sender, PaintEventArgs e)
-        {
-           
-        }
 
 
-        public void SetWindowRegion()
-        {
-            GraphicsPath FormPath;
-            FormPath = new GraphicsPath();
-            Rectangle rect = new Rectangle(0, 0, this.Width, this.Height);//this.Left-10,this.Top-10,this.Width-10,this.Height-10);                 
-            FormPath = GetRoundedRectPath(rect, 30);
-            this.Region = new Region(FormPath);
-        }
-        private GraphicsPath GetRoundedRectPath(Rectangle rect, int radius)
-        {
-            int diameter = radius;
-            Rectangle arcRect = new Rectangle(rect.Location, new Size(diameter, diameter));
-            GraphicsPath path = new GraphicsPath();
-            //左上角
-            path.AddArc(arcRect, 180, 90);
-            //   右上角   
-            arcRect.X = rect.Right - diameter;
-            path.AddArc(arcRect, 270, 90);
-            //右下角
-            arcRect.Y = rect.Bottom - diameter;
-            path.AddArc(arcRect, 0, 90);
-            //   左下角   
-            arcRect.X = rect.Left;
-            path.AddArc(arcRect, 90, 90);
-            path.CloseFigure();
-            return path;
-        }
 
+        
 
     }
 }
