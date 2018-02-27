e1b9b9e5fb7a84ff3c29938a9997ffe5c3eebd39
jingjiajie <932166095@qq.com>
2018-02-27 11:43:26 +0800

    tmp

--- UI/FormMain/FormMain.Designer.cs
+++ UI/FormMain/FormMain.Designer.cs
@@ -142,9 +142,9 @@ namespace WMS.UI
             this.label1.Location = new System.Drawing.Point(76, 14);
             this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(856, 57);
+            this.label1.Size = new System.Drawing.Size(932, 57);
             this.label1.TabIndex = 0;
-            this.label1.Text = "安途丰达WMS仓库管理系统 测试版本-210";
+            this.label1.Text = "安途丰达WMS仓库管理系统 V1.0 - Build 211";
             // 
             // panel1
             // 
